55ebcd8fd90d61bde290eca0ec852e017445521f
David Maximiliano Sosa <maxi3390@gmail.com>
2013-03-15 16:35:55 -0300

    Empezado el juego y removido archivos innecesarios

---
+++ Pooyan/Pooyan/Pooyan/Animation.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pooyan
+{
+    class Animation
+    {
+        // Textura donde estan TODOS los sprites en orden y donde todos tienen el MISMO ANCHO
+        Texture2D TexturaSprites;
+
+        // Escala que se aplica a cada sprite
+        float Escala;
+
+        // Cuanto tiempo pasó desde el último cambio de frames? (Se usa con la variable siguiente)
+        int TiempoTranscurrido;
+
+        // Cuanto tiempo transcurre entre frame y frame? Esto siempre va a ser una subestimación
+        int TiempoEntreFrames;
+
+        // Cuantos frames hay en total? El ancho total de la imagen debería ser esta variable * ancho de cada frame
+        int FramesEnLaImagen;
+
+        // El índice del frame que se está mostrando en un momento dado
+        int FrameActual;
+
+        // Esto es porque se pueden aplicar efectos de color a los frames, como que se ponga rojo si le pegan.
+        Color color;
+
+        // La parte de la textura que se quiere dibujar
+        Rectangle RectanguloOrigen = new Rectangle();
+
+        // El area donde se quiere dibujar el frame en la ventana
+        Rectangle RectanguloDestino = new Rectangle();
+
+        // Útiles para pasar de un frame a otro dentro de una imagen con muchos sprites o frames
+        public int AnchoFrame;
+        public int AltoFrame;
+
+        public bool Activo;
+
+        // Determina si se repite por siempre la animación
+        public bool Repetir;
+
+        public Vector2 Posicion;
+
+        public void Inicializar(Texture2D texture, Vector2 position,
+            int frameWidth, int frameHeight, int frameCount,
+            int frametime, Color color, float scale, bool looping)
+        {
+            this.color = color;
+            this.AnchoFrame = frameWidth;
+            this.AltoFrame = frameHeight;
+            this.FramesEnLaImagen = frameCount;
+            this.TiempoEntreFrames = frametime;
+            this.Escala = scale;
+
+            Repetir = looping;
+            Posicion = position;
+            TexturaSprites = texture;
+
+            TiempoTranscurrido = 0;
+            FrameActual = 0;
+
+            Activo = true;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (!Activo) return;
+
+            // Se actualiza el tiempo transcurrido, gameTime ya viene desde Game1 y se calcula solo
+            TiempoTranscurrido += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            // Si el tiempo transcurrido es mayor al tiempo entre frame y frame, hay que cambiarlo y resetearlo
+            if (TiempoTranscurrido > TiempoEntreFrames)
+            {
+                FrameActual++;
+                // Si el frame actual es igual a la cantidad total de frames, se resetea el actual
+                if (FrameActual == FramesEnLaImagen)
+                {
+                    FrameActual = 0;
+                    if (!Repetir) Activo = false;
+                }
+                TiempoTranscurrido = 0;
+            }
+
+            // El frame correcto se saca de multiplicar el frame actual por el ancho de cada sprite
+            // O sea que tienen que ser todos iguales
+            RectanguloOrigen = new Rectangle(FrameActual * AnchoFrame, 0, AnchoFrame, AltoFrame);
+
+            // El frame correcto se saca de multiplicar el frame actual por el ancho de cada sprite
+            // O sea que tienen que ser todos iguales
+            RectanguloDestino = new Rectangle((int)Posicion.X - (int)(AnchoFrame * Escala) / 2,
+            (int)Posicion.Y - (int)(AltoFrame * Escala) / 2,
+            (int)(AnchoFrame * Escala),
+            (int)(AltoFrame * Escala));
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (Activo)
+                spriteBatch.Draw(TexturaSprites, RectanguloDestino, RectanguloOrigen, color);
+        }
+    }
+}
---
+++ Pooyan/Pooyan/Pooyan/Personaje.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pooyan
+{
+    class Personaje
+    {
+        Vector2 DireccionDisparos;
+        // Disparos
+        public Texture2D TexturaProyectil;
+        public List<Projectil> Proyectiles;
+        SoundEffect SonidoLaser;
+        // El rate de disparo del personaje
+
+        public Animation Animacion;
+        public Vector2 Posicion;
+        public bool Activo;
+        float velocidadMovimiento = 8.0f;
+
+        // Referencia al GraphicsDevice principal del juego.
+        GraphicsDevice graphicsDevice;
+
+        public int Ancho
+        {
+            get { return Animacion.AnchoFrame; }
+        }
+
+        public int Alto
+        {
+            get { return Animacion.AltoFrame; }
+        }
+
+        public void Inicializar(Animation animation, Vector2 position, ContentManager content, GraphicsDevice graphicsDevice)
+        {
+            this.DireccionDisparos = new Vector2(15, 0);
+            this.graphicsDevice = graphicsDevice;
+            Animacion = animation;
+            Posicion = position;
+            Activo = true;
+
+            // Proyectiles
+            //Proyectiles = new List<Projectil>();
+            //TexturaProyectil = content.Load<Texture2D>("laser");
+            //SonidoLaser = content.Load<SoundEffect>("sound/laserFire");
+        }
+
+        public void Disparar(int type, GameTime gameTime)
+        {
+            TimeSpan delayShoot = gameTime.TotalGameTime; // -TiempoDeUltimoDisparo[type];
+
+            //if (delayShoot < DisparosFrecuencia[type])
+            //    return;
+            //TiempoDeUltimoDisparo[type] = gameTime.TotalGameTime;
+            //Trace.WriteLine("delayShoot: " + delayShoot.ToString());
+
+            Proyectiles.Add(new Projectil(graphicsDevice.Viewport, TexturaProyectil, Posicion, DireccionDisparos));
+            SonidoLaser.Play();
+        }
+
+        public void Update(GameTime gameTime, KeyboardState keyboardState, GraphicsDevice graphics, Vector2 DireccionDisparos)
+        {
+            Animacion.Posicion = Posicion;
+            Animacion.Update(gameTime);
+
+            //ActualizarProyectiles();
+
+            // Teclado / Dpad
+            /*if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                Posicion.X -= velocidadMovimiento;
+            }
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                Posicion.X += velocidadMovimiento;
+            }*/
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                Posicion.Y -= velocidadMovimiento;
+            }
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                Posicion.Y += velocidadMovimiento;
+            }
+
+            // Evitar que el personaje salga de la ventana
+            Posicion.X = MathHelper.Clamp(Posicion.X, 0, graphics.Viewport.Width - Ancho);
+            Posicion.Y = MathHelper.Clamp(Posicion.Y, 0, graphics.Viewport.Height - Alto);
+
+            if (keyboardState.IsKeyDown(Keys.A))
+                Disparar(0, gameTime);
+        }
+
+       /* public void ActualizarProyectiles()
+        {
+            // Update the Projectiles
+            for (int i = Proyectiles.Count - 1; i >= 0; i--)
+            {
+                Proyectiles[i].Update();
+                if (Proyectiles[i].Activo == false)
+                {
+                    Proyectiles.RemoveAt(i);
+                }
+            }
+        }*/
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            /*foreach (var p in Proyectiles)
+            {
+                p.Draw(spriteBatch);
+            }*/
+
+            Animacion.Draw(spriteBatch);
+        }
+    }
+}
